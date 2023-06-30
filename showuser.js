e40bef63223d8fe2095c37d1f1fe1c627ce951af
BeCare666 <becare.fr.ge@gmail.com>
2023-06-30 20:36:32 +0100

    button to callback add

--- showuser.js
+++ showuser.js
@@ -1,6 +1,2 @@
-const UserConnectuser = localStorage.getItem("unserconnect");
-alert(UserConnectuser)
-var BacktoprofilID = document.getElementById('backtoprofilID');
-    if(UserConnectuser){
-        BacktoprofilID.style.display = "block"
-}
+
+   
